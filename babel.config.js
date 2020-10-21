14a923eea8902ad6e20325327fb69a3a2603d6fb
bill.mark <2286449127@qq.com>
2020-10-21 17:26:45 +0800

    增加babel配置

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          "corejs": "3", // 指定core-js的版本，2或者3，这里我们用最新版3
+          "useBuiltIns": "usage", // usage是最佳实践，会按需把core-js和regenerator引入（所谓按需就是按下面的target和编译的js用到的es6语法来判断）
+          "targets": {
+            "browsers": [
+              "> 1%",
+              "last 2 versions",
+              "not ie <= 8"
+            ]
+          }
+        },
+      ]
+    ],
+  };
